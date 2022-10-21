00ea9d36f539eea5c9ad4dfd096814ac77bfa5c9
Liench Volodymyr <volodymyr.lench@binary-studio.com>
2022-10-21 14:13:33 +0300

    move test instance to the app

--- test/sql/3-grant-to-hackolade-user.sql
+++
@@ -1,5 +0,0 @@
--- GRANT ALL PRIVILEGES ON  mysql.proc to 'hackolade'@'%';
--- GRANT ALL PRIVILEGES ON  mysql.func to 'hackolade'@'%';
-
-GRANT ALL PRIVILEGES ON sakila.* TO 'hackolade'@'%' WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON sakila.* TO 'root'@'%' WITH GRANT OPTION;
