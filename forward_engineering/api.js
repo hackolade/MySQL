3b5ffaf8320edb0586f0c3a2119bd7b445d4ec4f
yevhenii-moroziuk <yevhen.moroziuk@hackolade.com>
2025-05-16 10:49:27 +0300

    Revert "HCK-11091: Move the log of the start to the studio (#46)"
    This reverts commit e40153fa74004feae1c8b29cf80ed5836b437350.

--- forward_engineering/api.js
+++ forward_engineering/api.js
@@ -12,6 +12,9 @@ module.exports = {
 		callback(null, '');
 	},
 	applyToInstance(connectionInfo, logger, callback, app) {
+		logger.clear();
+		logger.log('info', connectionInfo, 'connectionInfo', connectionInfo.hiddenKeys);
+
 		applyToInstanceHelper
 			.applyToInstance(connectionInfo, logger, app)
 			.then(result => {
